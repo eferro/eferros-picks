9cb9e108d459eb6c2e101a9fc2e9fbe3f6cb7121
Eduardo Ferro <eduardo.ferro@clarity.ai>
2025-01-06 23:22:57 +0100

    Generate the index by speaker

--- scripts/generate-awesome-talks.py
+++ scripts/generate-awesome-talks.py
@@ -22,16 +22,31 @@ def generate_awesome_talks():
         "talks_per_speaker": defaultdict(int)
     }
 
+    talks_by_speaker = defaultdict(list)
+
     for talk in awesome_talks:
         for topic in talk.get("Topics_Names", []):
             stats["talks_per_topic"][topic] += 1
         for speaker in talk.get("Speakers_Names", []):
             stats["talks_per_speaker"][speaker] += 1
+            talks_by_speaker[speaker].append(talk)
 
     stats["talks_per_topic"] = dict(sorted(stats["talks_per_topic"].items(), key=lambda item: item[1], reverse=True))
     stats["talks_per_speaker"] = dict(sorted(stats["talks_per_speaker"].items(), key=lambda item: item[1], reverse=True))
 
+    awesome_talks_by_speaker = defaultdict(list)
+    for speaker, talks in talks_by_speaker.items():
+        if len(talks) > 1:
+            awesome_talks_by_speaker[speaker] = talks
+        else:
+            awesome_talks_by_speaker["others"].extend(talks)
+
+    awesome_talks_by_speaker = dict(sorted(awesome_talks_by_speaker.items(), key=lambda item: (len(item[1]), item[0] != "others"), reverse=True))
+
     with open('database/awesome-talks-stats.json', 'w') as file:
         json.dump(stats, file, indent=4)
+
+    with open('database/awesome-talks-by-speaker.json', 'w') as file:
+        json.dump(awesome_talks_by_speaker, file, indent=4)
 
 generate_awesome_talks()
